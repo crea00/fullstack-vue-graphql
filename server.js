76ca3749240f21cfb4d096a34b65b8701ef8e740
Jerome <654321su@gmail.com>
2019-01-14 16:37:46 +0900

    Create Vue Client with Vue-CLI 3

--- server.js
+++ server.js
@@ -3,15 +3,17 @@ const mongoose = require('mongoose');
 const fs = require('fs');
 const path = require('path');
 
+// Import typeDefs and resolvers
 const filePath = path.join(__dirname, 'typeDefs.gql');
 const typeDefs = fs.readFileSync(filePath, 'utf-8');
 const resolvers = require('./resolvers');
 
+// Import Environment Variables and Mongoose Models
 require('dotenv').config();
 const User = require('./models/User.js');
 const Post = require('./models/Post.js');
 
-// Connect to mongoose
+// Connect to MLab Database
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => console.log('DB Connected!'))
@@ -19,6 +21,7 @@ mongoose
 // Set userCreateIndex
 mongoose.set('useCreateIndex', true);
 
+// Create Apollo/GraphQL server using typeDefs, resolvers, and context object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
