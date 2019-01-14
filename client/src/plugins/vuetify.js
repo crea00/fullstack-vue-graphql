beb57407725193733b3dda6e3c06156a4dd4564c
Jerome <654321su@gmail.com>
2019-01-15 00:48:49 +0900

    Install Vuetify Plugin and Generating a Theme

---
+++ client/src/plugins/vuetify.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import colors from 'vuetify/es5/util/colors'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.teal.lighten1,
+    secondary: colors.teal.lighten3,
+    accent: colors.red.lighten1,
+    error: colors.red.accent1,
+    warning: colors.amber.base,
+    info: colors.blue.accent3,
+    success: colors.green.darken4
+  }
+})
