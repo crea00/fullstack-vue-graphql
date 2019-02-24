0149d9599a0000b4b5c71846b0871811f3077807
Jerome <654321su@gmail.com>
2019-02-24 19:35:46 +0900

    Fire getPosts Action with Vuex

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <v-container text-xs-center v-if="getPosts">
-    <v-flex xs12>
-      <v-carousel v-bind="{ 'cycle': true }" interval="3000">
-        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
-          <h1 id="carousel__title">{{ post.title }}</h1>
+  <v-container
+    text-xs-center
+  >
+    <!-- <v-flex xs12>
+      <v-carousel
+        v-bind='{ 'cycle': true }'
+        interval='3000'
+      >
+        <v-carousel-item
+          v-for='post in getPosts'
+          :key='post._id'
+          :src='post.imageUrl'
+        >
+          <h1 id='carousel__title'>{{ post.title }}</h1>
         </v-carousel-item>
       </v-carousel>
-    </v-flex>
+    </v-flex> -->
   </v-container>
 </template>
 
@@ -15,29 +24,16 @@ import { gql } from 'apollo-boost';
 
 export default {
   name: 'home',
-  apollo: {
-    getPosts: {
-      query: gql`
-        query {
-          getPosts {
-            _id
-            title
-            imageUrl
-            description
-            likes
-          }
-        }
-      `,
-      result(args) {
-        console.dir(args)
-      },
-      error(err) {
-        console.log(`[ERROR!!] ${err}`);
-        console.dir(err);
-      }
+  created() {
+    this.handleGetCarouselPosts();
+  },
+  methods: {
+    handleGetCarouselPosts() {
+      // Reach out to Vuex store, fire action gets posts for carousel
+      this.$store.dispatch('getPosts');
     }
   }
-}
+};
 </script>
 
 <style>
--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,35 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+import { defaultClient as apolloClient } from './main';
+import { gql } from 'apollo-boost';
+
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
+  state: {},
+  mutations: {},
   actions: {
-
+    getPosts: () => {
+      // Use ApolloClient to fire getPosts query
+      apolloClient
+        .query({
+          query: gql`
+            query {
+              getPosts {
+                _id
+                title
+                imageUrl
+              }
+            }
+          `
+        })
+        .then(data => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }
   }
-})
+});
