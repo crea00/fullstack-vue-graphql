bf75b6cd7e1fb7551d741e0c9254d1e8a5acf8e0
Jerome <654321su@gmail.com>
2019-03-02 14:26:39 +0900

    Sign Token and Return it Upon Signin/Signup

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,10 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const createToken = (user, secret, expiresIn) => {
+  const { username, email } = user;
+  return jwt.sign({ username, email }, secret, { expiresIn });
+};
 
 module.exports = {
   Query: {
@@ -36,7 +42,7 @@ module.exports = {
       if (!isValidPassword) {
         throw new Error('Invalid password');
       }
-      return user;
+      return { token: createToken(user, process.env.SECRET, '1hr' ) };
     },
     signupUser: async (_, { username, email, password }, { User }) => {
       const user = await User.findOne({ username });
@@ -48,7 +54,7 @@ module.exports = {
         email,
         password
       }).save();
-      return newUser;
+      return { token: createToken(newUser, process.env.SECRET, '1hr' ) };
     }
   }
 };
